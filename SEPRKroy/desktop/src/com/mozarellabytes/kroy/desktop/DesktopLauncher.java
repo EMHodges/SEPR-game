3b512f09d499d4bba8f6afcc0061e8dd0abb6732
Emilien <emilien.bev.com@gmail.com>
2019-10-24 17:52:40 +0100

    Initial commit of libGDX template project (IntelliJ)

---
+++ SEPRKroy/desktop/src/com/mozarellabytes/kroy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mozarellabytes.kroy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mozarellabytes.kroy.KroyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KroyMain(), config);
+	}
+}
