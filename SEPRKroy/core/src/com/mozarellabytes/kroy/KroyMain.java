3324f0101b6c1308c3fff5720256d2ade25adcb2
dan <db1218@york.ac.uk>
2019-10-28 13:36:32 +0000

    Messed around with controls/input

--- SEPRKroy/core/src/com/mozarellabytes/kroy/KroyMain.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/KroyMain.java
@@ -2,20 +2,23 @@ package com.mozarellabytes.kroy;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class KroyMain extends ApplicationAdapter {
+
 	// The SpriteBatch is a special class that is used to draw 2D images
 	private SpriteBatch batch;
 	private Texture img;
-	private Texture bucketImage;
+	private Texture donutImage;
 	private OrthographicCamera camera;
 
-	private Rectangle bucket;
+	private Rectangle donut;
 
 	// Method called once when the application is created.
 	@Override
@@ -23,32 +26,40 @@ public class KroyMain extends ApplicationAdapter {
 		camera = new OrthographicCamera();
 		batch = new SpriteBatch();
 		img = new Texture("logo.png");
-		bucketImage = new Texture("bucket.jpg");
+		donutImage = new Texture("donut.png");
 
-		camera.setToOrtho(false, Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
+		camera.setToOrtho(false, Gdx.graphics.getDisplayMode().width/2, Gdx.graphics.getDisplayMode().height/2);
 
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 32;
-		bucket.height = 32;
+		donut = new Rectangle();
+		donut.x = Gdx.graphics.getDisplayMode().width/2 - donutImage.getWidth()/2;
+		donut.y = 20;
+		donut.width = 32;
+		donut.height = 32;
 
-		Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
+		Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width/2, Gdx.graphics.getDisplayMode().height/2);
 		Gdx.app.log("Log", "this is a log message");
 	}
 
 	// Method called by the game loop from the application every time rendering should be performed. Game logic updates are usually also performed in this method.
 	@Override
 	public void render () {
-		camera.update();
 		Gdx.gl.glClearColor(51/255f, 34/255f, 99/255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, Gdx.graphics.getWidth()/2 - img.getWidth()/2,Gdx.graphics.getHeight()/2 - img.getHeight()/2);
-		batch.draw(bucketImage, Gdx.graphics.getWidth()/2 - bucketImage.getWidth()/2, 100, camera.viewportWidth/10, camera.viewportHeight/10);
+		batch.draw(donutImage, donut.x, donut.y, camera.viewportWidth/10, camera.viewportHeight/10);
 		batch.end();
+
+		// movement input for donut
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) donut.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) donut.x += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) donut.y += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) donut.y -= 200 * Gdx.graphics.getDeltaTime();
+
 	}
 
 	// Called when the application is destroyed. It is preceded by a call to pause().
