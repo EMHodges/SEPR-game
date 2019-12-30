635b291026108ad8a51bf32f2a5a99299809128b
dan <db1218@york.ac.uk>
2019-12-30 16:44:30 +0000

    Working JUnit Test (for me anyway)

---
+++ SEPRKroy/core/test/com/mozarellabytes/kroy/Entities/FortressTest.java
@@ -0,0 +1,14 @@
+package com.mozarellabytes.kroy.Entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FortressTest {
+
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(1, 1);
+    }
+
+}
